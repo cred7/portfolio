31a379335ad41270cedf431a2591d4e50540462f
elvis <omondielvis.owuor@gmail.com>
2026-04-20 17:47:02 +0300

    first-updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen flex flex-col overflow-hidden bg-black">
         {children}
+        <Analytics />
       </body>
     </html>
   );
