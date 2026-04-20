6ccd0f2b6021a7db2c4ef2c3610df98f226f22f0
elvis <omondielvis.owuor@gmail.com>
2026-04-20 18:33:21 +0300

    first-update-scrollbar.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import LiveDateTime from "./Time";
 const Footer = async () => {
   return (
     <div className="h-full py-4 text-sm max-sm:text-xs font-mono text-center w-full flex justify-between items-center text-gray-600 p-1 max-w-5xl m-auto border-x border-green-600">
-      <p className=" text-gray-300 max-sm:hidden">#elvisomondi@owuor</p>
+      <p className=" text-gray-300 ">#elvisomondi@owuor</p>
       <div className="text-green-600">
         <LiveDateTime />
       </div>
