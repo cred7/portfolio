70a7168b80cb50f7b5a11ac6818b855a4aac38e0
elvis <omondielvis.owuor@gmail.com>
2026-04-20 18:44:18 +0300

    first-update-scrollbar.

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ import Header from "@/components/Header";
 const page = () => {
   return (
     <div className="h-screen w-screen flex m-0 p-0 bg-black">
-      <div className="flex flex-col w-full h-full px-2">
+      <div className="flex flex-col w-full h-full px-2 ">
         {/* header */}
-        <div className="flex w-full h-[13%] p-1 border-b border-green-600">
+        <div className="flex w-full h-[13%]  p-1 border-b border-green-600">
           <Header />
         </div>
         {/* body */}
-        <div className="flex w-full h-[88%] p-1 overflow-hidden">
+        <div className="flex w-full h-[88%] max-sm:h-[85%] p-1 overflow-hidden">
           <Body />
         </div>
         {/* footer */}
-        <div className="flex w-full h-[9%] p-1 border-t border-green-600">
+        <div className="flex w-full h-[9%] max-sm:h-[11%] p-1 border-t border-green-600">
           <Footer />
         </div>
       </div>
